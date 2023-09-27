498a6c8ce5fa65e759bc59083ff3349d3f580694
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2023-09-27 19:57:41 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// Add a listener for the DOMContentLoaded event.
+document.addEventListener("DOMContentLoaded", function() {
+    // Update the text on the page.
+    document.querySelector("p").textContent = "This is really cool!";
+  });
+  
+  
